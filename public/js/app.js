77c40c9cd1b2f3094f6ca73bef7274a9f87f9d27
medreda007 <karimmedreda@gmail.com>
2024-01-17 20:31:36 +0100

    sec4 done

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+let nav = document.querySelector('.navigation');
+let navBtn = document.querySelector(".nav-btn-toggler")
+navBtn.addEventListener('click',() =>{
+    nav.classList.toggle("show-nav")
+})
