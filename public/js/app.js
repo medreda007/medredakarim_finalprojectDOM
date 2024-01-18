3d223333ee02d624bac2c3c960ca58e6fcdf230d
medreda007 <karimmedreda@gmail.com>
2024-01-18 08:45:17 +0100

    first carousel done

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 let nav = document.querySelector('.navigation');
 let navBtn = document.querySelector(".nav-btn-toggler")
-navBtn.addEventListener('click',() =>{
+navBtn.addEventListener('click', () => {
     nav.classList.toggle("show-nav")
+})
+
+
+let signIn = document.querySelector(".sign-in")
+let logInForm = document.querySelector(".log-in-form")
+
+let logIn = document.querySelector(".log-in")
+let signInForm = document.querySelector(".sign-in-form")
+
+signIn.addEventListener('click', () => {
+    logInForm.classList.add("d-none")
+    signInForm.classList.remove("d-none")
+})
+
+logIn.addEventListener('click', () => {
+    signInForm.classList.add("d-none")
+    logInForm.classList.remove("d-none")
 })
